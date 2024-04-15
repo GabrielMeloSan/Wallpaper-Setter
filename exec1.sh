6459427826b69c9f3a23cb56c1219c840b89f798
Gabriel Melo Santos <gabriel.melo.santos.15@gmail.com>
2024-04-15 17:23:13 -0300

    v0.1

---
+++ exec1.sh
@@ -0,0 +1,16 @@
+#!
+
+#Github: GabrielMeloSan
+ 
+ #Put your file path here
+cd #/your/file/path
+
+#installing Nitrogen
+sudo apt install nitrogen
+
+#Running Nitrogen Code
+nitrogen --set-zoom-fill #name_of_your_file.png
+
+echo "\n\n\nWallpaper setted! \n\nGithub: GabrielMeloSan"
+
+sleep 3
